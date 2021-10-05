1ca89a96d66504bf23b31eddc5e42874dc209255
trece <abeltrece@gmail.com>
2021-10-05 15:32:35 -0700

    Iniciando dev Spring

---
+++ src/main/java/com/practica/dev/PracticaDeProyectoDevApplication.java
@@ -0,0 +1,13 @@
+package com.practica.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaDeProyectoDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaDeProyectoDevApplication.class, args);
+	}
+
+}
